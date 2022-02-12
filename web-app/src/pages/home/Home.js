b83e81bcc9d1a6c98bedc17d0f2696952116e742
sreyom31 <sreyom31@gmail.com>
2022-02-12 13:32:17 +0530

    landing page
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

---
+++ web-app/src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import LandingPage from "./landingPage/LandingPage";
+import "./Home.css";
+import { Fragment } from "react";
+
+const Home = () => {
+  return (
+    <Fragment>
+      <LandingPage />
+    </Fragment>
+  );
+};
+
+export default Home;
