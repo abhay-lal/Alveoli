7906dd9e93e2de40c4c81adfbf312a6007fba7b6
sreyom31 <sreyom31@gmail.com>
2022-02-12 13:45:01 +0530

    about page design
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- web-app/src/pages/home/Home.js
+++ web-app/src/pages/home/Home.js
@@ -1,11 +1,13 @@
 import LandingPage from "./landingPage/LandingPage";
 import "./Home.css";
+import About from "./about/About";
 import { Fragment } from "react";
 
 const Home = () => {
   return (
     <Fragment>
       <LandingPage />
+      <About />
     </Fragment>
   );
 };
