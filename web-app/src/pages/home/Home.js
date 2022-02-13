665f306f17c8e79605e47fbecd32a2ec76534cf0
sreyom31 <sreyom31@gmail.com>
2022-02-13 05:53:14 +0530

    home routes
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- web-app/src/pages/home/Home.js
+++ web-app/src/pages/home/Home.js
@@ -2,12 +2,16 @@ import LandingPage from "./landingPage/LandingPage";
 import "./Home.css";
 import About from "./about/About";
 import { Fragment } from "react";
+import Categories from "./categories/Categories";
+import Problem from "./problems/Problem";
 
 const Home = () => {
   return (
     <Fragment>
       <LandingPage />
       <About />
+      <Categories />
+      <Problem />
     </Fragment>
   );
 };
