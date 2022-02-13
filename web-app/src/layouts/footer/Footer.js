35dc0dbb00ddc85482809d9480f0b713198e54a1
sreyom31 <sreyom31@gmail.com>
2022-02-13 06:04:01 +0530

    footer design
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- web-app/src/layouts/footer/Footer.js
+++ web-app/src/layouts/footer/Footer.js
@@ -1,76 +1,14 @@
 import "./Footer.css"
 import React from 'react';
 import {
-  MDBFooter,
-  MDBContainer,
-  MDBCol,
-  MDBRow,
-  MDBIcon
+  MDBFooter
 } from 'mdb-react-ui-kit';
 const Footer = () => {
   return (
-    <MDBFooter className='text-center text-white' style={{ backgroundColor: '#d8efed' }}>
-      <MDBContainer className='pt-4'>
-        <section className='mb-4'>
-          <a
-            className='btn btn-link btn-floating btn-lg text-dark m-1'
-            href='#!'
-            role='button'
-            data-mdb-ripple-color='dark'
-          >
-            <MDBIcon fab className='fab fa-facebook-f' />
-          </a>
-
-          <a
-            className='btn btn-link btn-floating btn-lg text-dark m-1'
-            href='#!'
-            role='button'
-            data-mdb-ripple-color='dark'
-          >
-            <MDBIcon fab className='fa-twitter' />
-          </a>
-
-          <a
-            className='btn btn-link btn-floating btn-lg text-dark m-1'
-            href='#!'
-            role='button'
-            data-mdb-ripple-color='dark'
-          >
-            <MDBIcon fab className='fa-google' />
-          </a>
-
-          <a
-            className='btn btn-link btn-floating btn-lg text-dark m-1'
-            href='#!'
-            role='button'
-            data-mdb-ripple-color='dark'
-          >
-            <MDBIcon fab className='fa-instagram' />
-          </a>
-
-          <a
-            className='btn btn-link btn-floating btn-lg text-dark m-1'
-            href='#!'
-            role='button'
-            data-mdb-ripple-color='dark'
-          >
-            <MDBIcon fab className='fa-linkedin' />
-          </a>
-
-          <a
-            className='btn btn-link btn-floating btn-lg text-dark m-1'
-            href='#!'
-            role='button'
-            data-mdb-ripple-color='dark'
-          >
-            <MDBIcon fab className='fa-github' />
-          </a>
-        </section>
-      </MDBContainer>
-
-      <div className='text-center text-dark p-3' style={{ backgroundColor: '#d8efed' }}>
-        © 2022 Copyright:
-        <a className='text-dark' href='/'>
+    <MDBFooter className='text-center text-white container-fluid foo' style={{ backgroundColor: '#d8efed' }}>
+      <div className='text-center text-dark p-3 fw-bolder' style={{ backgroundColor: '#d8efed' }}>
+        © 2022 Copyright: &nbsp;
+        <a className='text-dark' href='/' style={{textDecoration: "none"}}>
         Alveoli
         </a>
       </div>
